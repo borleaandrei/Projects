d6f997a4c80486dc6bd0c49d20e3af1b74fde5e2
Dany9966 <vincze.daniel96@gmail.com>
2018-11-07 14:03:25 +0200

    Queue functionality ready to roll!     - concurrency problems resolved     - broker message sending and receiving are fully functional

--- src/com/rockets/Queue.java
+++ src/com/rockets/Queue.java
@@ -8,22 +8,39 @@ public class Queue {
     private String destination;             // destination from the header or topic if specified;
                                             // which message goes to which queue will be decided by the Broker,
                                             // using this field
+    private static int MAX_QUEUE_LENGTH = 5; // maximum number of messages to be put in queues
+    private boolean receiving;
 
 
     public Queue(String d) {
         this.messages = new ArrayList<Message>();
         this.destination = d;
+        this.receiving = false;
     }
 
     // adds message to queue
-    public void add(Message message) {
+    public synchronized void add(Message message) {
+        while (receiving) {
+            try {
+
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        receiving = true;
+        if (getLength() >= MAX_QUEUE_LENGTH) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         messages.add(message);
+        receiving = false;
+        //not receiving any more messages, notifying threads
+        notifyAll();
     }
-//
-//    public void deleteMessage(int brokerTime) {
-//        // TODO: delete message after time
-//    }
-//
 
     public String toString() {
         String ret = "";
@@ -35,5 +52,21 @@ public class Queue {
 
     public String getDestination() {
         return this.destination;
+    }
+
+    public synchronized void removeFirst() {
+        while(getLength() == 0) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        this.messages.remove(0);
+        notifyAll();
+    }
+
+    public int getLength() {
+        return messages.size();
     }
 }
