4d9611ece73f80912b72d87615f6381fdfe63e16
Borlea Andrei <andrei.borlea@busymachines.com>
2018-11-11 12:39:00 +0200

    added implementation for synchronized map and queue

---
+++ src/com/rockets/topics/SynchronizedQueueImpl.java
@@ -0,0 +1,28 @@
+package com.rockets.topics;
+
+import java.util.ArrayList;
+
+public class SynchronizedQueueImpl<E> {
+
+    private ArrayList<E> queue;
+
+    public SynchronizedQueueImpl() {
+        queue = new ArrayList<>();
+    }
+
+    public SynchronizedQueueImpl(ArrayList<E> queue) {
+        this.queue = queue;
+    }
+
+    public synchronized void put(E element) {
+        queue.add(element);
+    }
+
+    public synchronized ArrayList<E> toArrayList() {
+        return queue;
+    }
+
+    public synchronized int size() {
+        return queue.size();
+    }
+}
